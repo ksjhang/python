47292008fcafdcfab31cc7839003a8f00af73bda
kyoungson-jhang <sun@cnu.ac.kr>
2016-08-25 21:22:40 +0900

    index correction

--- selection-sort.py
+++ selection-sort.py
@@ -15,5 +15,5 @@ vlist=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 7
 vlist = vlist[::-1];
 print( vlist)
 
-selection(vlist,0,len(vlist)-1)
+selection(vlist,0,len(vlist))
 print(vlist)
